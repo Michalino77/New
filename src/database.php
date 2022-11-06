2d22d54bb826db3a751ffa99836ed4718878f111
michal <abc@example.com>
2022-11-06 15:30:27 +0100

    create

--- src/database.php
+++ src/database.php
@@ -16,17 +16,35 @@ class Database
     {
         try {
             $this->valiadeteConfig($config);
-            $dsn = "mysql:dbname=1config['database']};host={$config['host']}";
-            $connection = new PDO(
-                $dsn,
-                $config['user'],
-                $config['password'],
-            );
-        catch (PDOException $e) {
+            $this->createConnection($config);
+        
+        } catch (PDOException $e) {
             throw new StorageException('Connection error');
         }
      }
 
+     public function createNote(array $data): void
+     {
+        try {
+            $title = $this->conn->quote($data['title']);
+            $description = $this->conn->quote($data['description']);
+            $created = date ('Y-m-d H:i:s');
+        } catch (Throwable $e) {
+            throw new StorageException('Nie udało się utworzyć nowej notatki', 400, $e);
+        }
+     }
+     private function createConnection(array $config): void
+     {
+        $dsn = "mysql:dbname={$config['database']};host={config['host']}";
+        $this->conn = new PDO(
+            $dsn,
+            $config['user'],
+            $config['password'],
+            [
+                POD::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+            );
+     }
         private function validateConfig(array $Config): void
         {
             if (empty($config['database']) || empty($config['user']) || empty($config['host'])) {
@@ -34,4 +52,4 @@ class Database
             }
         }
     }
-    }
+    
