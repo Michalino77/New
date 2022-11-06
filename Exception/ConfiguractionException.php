6534a91f20a5edc35e2751d72454db40b9142da4
michal <abc@example.com>
2022-11-06 14:13:21 +0100

    Changes

---
+++ Exception/ConfiguractionException.php
@@ -0,0 +1,13 @@
+<?php
+
+declarate(strict_types=1);
+
+namespace App\Exception;
+
+require_once('AppException.php');
+
+use App\Exception\AppException;
+
+class ConfigurationException extends AppException
+{
+}
--- src/database.php
+++ src/database.php
@@ -4,18 +4,34 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
 use PDO;
+use PDOExceptionn;
+use Throwable;
 
 class Database
 {
-    public function __construct(array $config)
+    public function_construct(array $config)
     {
-        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-        //$connection = new PDO(
-        //    $dsn,
-        //    $config['user'],
-        //    $config['password'],
-        // );
-        $connection = new PDO('sss');
+        try {
+            $this->valiadeteConfig($config);
+            $dsn = "mysql:dbname=1config['database']};host={$config['host']}";
+            $connection = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password'],
+            );
+        catch (PDOException $e) {
+            throw new StorageException('Connection error');
+        }
+     }
+
+        private function validateConfig(array $Config): void
+        {
+            if (empty($config['database']) || empty($config['user']) || empty($config['host'])) {
+                throw new ConfigurationException('Problem z konfiguracją - Skontaktuj sią z administratorm');
+            }
+        }
     }
-}
+    }
