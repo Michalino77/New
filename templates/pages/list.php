df9009c1dd3c8a3d8a115a449b28052cb72be4e6
michal <abc@example.com>
2022-11-14 19:47:11 +0100

    noteData

--- templates/pages/list.php
+++ templates/pages/list.php
@@ -1,27 +1,19 @@
-<b><?php echo $params['resultCreate'] ?? "" ?></b>
-<h3>nowa notatka</h3>
 <div>
-    <?php if ($params['created']) : ?>
-        <div>
-            <div>Tytuł: <?php echo $params['title'] ?></div>
-            <div>Opis: <?php echo $params['description'] ?> </div>
-        </div>
-    <?php else : ?>
+    <div class="message">
+        <?php
+        if (!empty{$params['before']}) {
 
-        <form action="/?action=create" class="note-form" method="post">
-            <ul>
-                <li>
-                    <label for="title">Tytuł <span class="required">*</span></label>
-                    <input type="text" name="title" id="title" class="filed-long">
-                </li>
-                <li>
-                    <label for="field5">Treść</label>
-                    <textarea name="description" id="field5" class="field-long field-textarea"></textarea>
-                </li>
-                <li>
-                    <input type="submit" value="Submit">
-                </li>
-            </ul>
-        </form>
-    <?php endif; ?>
-</div>
+
+            switch ($params['before']) {
+                case 'created':
+                    echo 'Notatka została utworzona!';
+                    break;
+            }
+        }
+        ?>
+    </div>
+    <b><?php
+        echo $params['resultList'] ?? ""; ?>
+    </b>
+    <h3>Lista notatek</h3>
+</div> 
