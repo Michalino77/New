92bd5f4d8513ed4286dcdb1d09b7c8e103267e2f
michal <abc@example.com>
2022-11-20 14:39:52 +0100

    add abstract

--- Exception/StorageException.php
+++ Exception/StorageException.php
@@ -1,6 +1,6 @@
 <?php
 
-declarate(strict_types=1);
+declare(strict_types=1);
 
 namespace App\Exception;
 
---
+++ src/NoteController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Exception\NotFoundException;
+
+require_once('AbstractController.php');
+
+class NoteController extends AbstractController
+{
+    public function createAction()
+    {
+        if ($this->request->hasPost()) {
+            $noteData = [
+                'title' => $this->request->postParam('title'),
+                'description' => $this->request->postParam('description'),
+            ];
+
+            $this->database->createNote($noteData);
+            header('Location: /?before=created');
+            exit;
+        }
+        $this->view->render('create');
+    }
+
+    public function showAction()
+    {
+        $noteId = (int) $this->request->getParam('id');
+
+        if (!$noteId) {
+            header('Location: /?error=missingNoteId');
+            exit;
+        }
+        try {
+            $note = $this->database->getNote($noteId);
+        } catch (NotFoundException $e) {
+            header('Location: /?error=noteNotFound');
+            exit;
+        }
+        $viewParams = [
+            'title' => 'Moja notatka',
+            'description' => 'Opis',
+            'note' => $note,
+        ];
+        $this->view->render('show', ['note' => $note]);
+    }
+
+    public function listAction()
+    {
+        $viewParams = [
+            'notes' => $this->database->getNotes(),
+            'before' => $this->request->getParam('before'),
+            'error' => $this->request->getParam('error'),
+        ];
+        $this->view->render('list', [
+            'notes' => $this->database->getNotes(),
+            'before' => $this->request->getParam('before'),
+            'error' => $this->request->getParam('error'),
+        ]);
+    }
+}
